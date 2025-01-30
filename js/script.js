c8ffdd49363e19d331c3c46979f9a43aa9329d11
adnansanipramana <adnansanipramana@gmail.com>
2025-01-30 09:51:41 +0700

    kalkulator BMI V1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // Ambil elemen yang diperlukan
 const bmiform = document.getElementById('bmiform');
 const resultsection = document.getElementById('resultsection');
+const genderresult = document.getElementById('genderresult');
 const bmresult = document.getElementById('bmresult');
 const bmcategory = document.getElementById('bmcategory');
-const diseasediagnosis = document.getElementById('diseasediagnosis');
-const solution = document.getElementById('solution');
+const diagnosis = document.getElementById('diagnosis');
 const resetbtn = document.getElementById('resetbtn');
 
 // Fungsi untuk menghitung BMI
@@ -22,32 +22,32 @@ function calculateBMI(event) {
 
     // Tentukan kategori BMI
     let category = '';
-    let diagnosis = '';
+    let diagnosisText = '';
     let solutionText = '';
 
     if (bmi < 18.5) {
         category = 'Kurus';
-        diagnosis = 'Anda memiliki berat badan kurang.';
+        diagnosisText = 'Anda memiliki berat badan kurang.';
         solutionText = 'Disarankan untuk meningkatkan asupan kalori dengan makan lebih banyak dan berfokus pada diet yang seimbang.';
     } else if (bmi >= 18.5 && bmi < 24.9) {
         category = 'Normal';
-        diagnosis = 'Berat badan Anda normal.';
+        diagnosisText = 'Berat badan Anda normal.';
         solutionText = 'Pertahankan gaya hidup sehat dan pola makan seimbang.';
     } else if (bmi >= 25 && bmi < 29.9) {
         category = 'Overweight (Kelebihan Berat Badan)';
-        diagnosis = 'Anda memiliki kelebihan berat badan.';
+        diagnosisText = 'Anda memiliki kelebihan berat badan.';
         solutionText = 'Disarankan untuk meningkatkan aktivitas fisik dan memperbaiki pola makan.';
     } else {
         category = 'Obesitas';
-        diagnosis = 'Anda mengalami obesitas.';
+        diagnosisText = 'Anda mengalami obesitas.';
         solutionText = 'Disarankan untuk berkonsultasi dengan ahli gizi dan memulai program penurunan berat badan yang sehat.';
     }
 
-    // Tampilkan hasil BMI dan kategori
+    // Tampilkan hasil BMI, kategori, dan jenis kelamin
+    genderresult.textContent = `Jenis Kelamin: ${gender === 'male' ? 'Laki-laki' : 'Perempuan'}`;
     bmresult.textContent = `BMI Anda adalah: ${bmi.toFixed(2)}`;
     bmcategory.textContent = `Kategori: ${category}`;
-    diseasediagnosis.textContent = `Diagnosis: ${diagnosis}`;
-    solution.textContent = `Solusi: ${solutionText}`;
+    diagnosis.textContent = `Diagnosis: ${diagnosisText} Solusi: ${solutionText}`;
 
     // Tampilkan section hasil dan scroll ke bagian hasil
     resultsection.style.display = 'block';
